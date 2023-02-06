94ffeade196ce7fcad2898f20d6dacdeace03b08
Shraddha <jadhavshradddha5@gmail.com>
2023-02-06 21:42:00 +0530

    Welcome to gambling world

---
+++ src/com/BridgeLabZ/Gambling_Simulator/Main.java
@@ -0,0 +1,19 @@
+package com.BridgeLabZ.Gambling_Simulator;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("As a Gambler, would start with a stake of $100 every day and bet $1 every game. If Yes then type Yes else No type:");
+        String answer= sc.next();
+        if(answer=="Yes")
+        {
+            System.out.println("Welcome to Gambling World");
+        }
+        else
+        {
+            System.exit(0);
+        }
+    }
+}
