421f872164c5cce29fdc31ed95cfe38a6b512780
Shraddha <jadhavshradddha5@gmail.com>
2023-02-06 21:55:18 +0530

    As a Gambler make

--- src/com/BridgeLabZ/Gambling_Simulator/Main.java
+++ src/com/BridgeLabZ/Gambling_Simulator/Main.java
@@ -5,7 +5,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        System.out.println("As a Gambler, would start with a stake of $100 every day and bet $1 every game. If Yes then type Yes else No type:");
+        int n, $stack, $goal;
+       /* System.out.println("As a Gambler, would start with a stake of $100 every day and bet $1 every game. If Yes then type Yes else No type:");
         String answer= sc.next();
         if(answer=="Yes")
         {
@@ -14,6 +15,10 @@ public class Main {
         else
         {
             System.exit(0);
-        }
+        }*/
+        System.out.println("As a Gambler make $1 bet so either win or loose $1:");
+        $stack = sc.nextInt();
+
+
     }
 }
